3b7afa0b82c4338589b8c1eee72b5f9208833888
Matt Quinn <mjq0533@gmail.com>
2022-02-22 19:52:03 -0600

    commit tree b9126cd561cefd5c626ff3fa786bc16fa9da2cf6

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,6 @@
 # Write your code below game_hash
+require 'pry'
+
 def game_hash
   {
     home: {
@@ -126,4 +128,38 @@ def game_hash
   }
 end
 
-# Write code here
+
+def num_points_scored(player_name)
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      if attribute == :players
+        data.each do |player|
+          if player[:player_name] == player_name
+            return player[:points]
+          end
+        end
+      end
+    end
+  end
+end
+
+
+def shoe_size(player_name)
+  game_hash.each do |location, team_data|
+    team_data[:players].each do |player|
+      if player[:player_name] == player_name
+        return player[:shoe]
+      end
+   end
+  end
+end
+
+def team_colors(team_name)
+  if team_name == 'Charlotte Hornets'
+    return game_hash[:away][:colors]
+  else return game_hash[:home][:colors]
+  end
+end
+
+
+
